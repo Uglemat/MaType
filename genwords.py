6542fb913655022c35ef1da700fc4749009ae90d
Prafulla Giri <pratheblackdiamond@gmail.com>
2018-01-01 12:15:27 +0545

    Modified the game to support case-sensitive words as well

--- genwords.py
+++ genwords.py
@@ -1,6 +1,6 @@
 import sys
 import random
-from string import ascii_lowercase
+from string import printable
 from collections import defaultdict
 
 dictfile = sys.argv[1]
@@ -10,10 +10,10 @@ words = defaultdict(set)
 
 
 with open(dictfile) as file:
-    wordlist = [w.lower() for w in file.read().split()]
+    wordlist = [w for w in file.read().split()]
 
 random.shuffle(wordlist)
-wordlist = filter(lambda w: all(c in ascii_lowercase for c in w), wordlist)
+wordlist = filter(lambda w: all(c in printable for c in w), wordlist)
 wordlist = wordlist[:1300]
 
 for word in wordlist:
